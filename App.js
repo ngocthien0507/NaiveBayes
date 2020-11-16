79bc565a15e96b2436940a7ae40c69bf1cef2cc2
ngocthien0507anttech <ngoc-thien.nguyen@ant-tech.eu>
2020-11-16 22:33:36 +0700

    add bayes naive v1

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ChatScreen } from './App/containers'
+
+
+const App = () => {
+
+  return (
+    <ChatScreen />
+  );
+};
+
+
+export default App;
---
+++ App/containers/ChatScreen/ChatScreen.js
@@ -0,0 +1,87 @@
+import React, { useState, useCallback, useEffect } from 'react'
+import {
+    SafeAreaView,
+    StyleSheet
+} from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
+
+// functions
+
+import bayes from '../../functions/BayesNaive/BayesNaiveFunction'
+
+var classifier = bayes()
+
+const ChatScreen = () => {
+    const [messages, setMessages] = useState([]);
+
+    const user = {
+        _id: 1,
+    }
+
+    const training = async () => {
+        // teach it a positive phrase
+
+        await classifier.learn('amazing, awesome movie!! Yeah!! Oh boy.', 'positive')
+        await classifier.learn('Sweet, this is incredibly, amazing, perfect, great!!', 'positive')
+        await classifier.learn('baby, honey, love, hug, beautiful', 'positive')
+        await classifier.learn('nice!, good, best, handsome', 'positive')
+
+        // teach it a negative phrase
+
+        await classifier.learn('terrible, shitty thing. Damn. Sucks!!', 'negative')
+        await classifier.learn('fuck, fucker. sucker. loser!!', 'negative')
+        await classifier.learn('nam, ngu, idiot. ass. sex!!', 'negative')
+        await classifier.learn('chat sex, tri soi than, soi than. ba doi. 3 doi!!', 'negative')
+        await classifier.learn('cac,dai,buom,chim,cu,lon', 'negative')
+        await classifier.learn('dick,pussy,penis,porn,sex', 'negative')
+    }
+
+    useEffect(() => {
+        training()
+        return setMessages([
+            {
+                _id: 1,
+                text: 'Hello developer',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+        ])
+    }, [])
+
+
+    const onSendMessage = useCallback(async (messages = []) => {
+        if (messages[0].text) {
+            console.log('text: ', messages[0].text)
+            let result = await classifier.categorize(messages[0].text)
+            result === 'negative' && alert('Nói bậy ít thôi :)')
+
+        }
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+    }, [])
+
+    return (
+        <>
+            <SafeAreaView style={styles.wrapper}>
+                <GiftedChat
+                    messages={messages}
+                    onSend={onSendMessage}
+                    user={user}
+                />
+            </SafeAreaView>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+
+});
+
+export default ChatScreen;
